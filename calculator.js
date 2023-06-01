478c14c72fd7adbe79fd7d19360105bc3d5a4d92
Jason Ruben <jsnrbn01@gmail.com>
2023-06-01 23:11:57 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 const output = document.querySelector('.output-value');
 const buttons = document.querySelectorAll('button');
-let operand1 = 0;
-let operand2 = null;
-let operator = null;
+
+let operation = [];
+const operatorArray = ['+', '-', '*', '/'];
 
 buttons.forEach(
     (button) => {
@@ -10,6 +10,8 @@ buttons.forEach(
             'click',
             (e) => {
                 let value = e.target.value;
+                let result;
+                let operatorIndex;
                 if (value === 'clear') {
                     output.innerText = '';
                     operand1 = '';
@@ -20,25 +22,38 @@ buttons.forEach(
                     output.innerText = string.slice(0, -1);
                 } else if (value === '+' || value === '-' || value === 'x' || value === '/') {
                     operator = value;
-                    operand1 = parseFloat(output.innerText);
-                    output.innerText = '';
+                    for ( let value of operatorArray) {
+                        const index = operation.indexOf(value)
+                        if ( index == value) {
+                            operatorIndex = index;
+                            break;
+                        }
+                    }
+                    let left = operation.slice(0, operatorIndex)
+                    operand1 = parseFloat(left.join(''));
+                    result = output.innerText += value;
+                    operation.push(result);
                 } else if (value === '.' && output.innerText.includes('.')) {
                     return;
                 } else if (value === '=') {
-
-                    operand2 = parseFloat(output.innerText);
-                    output.innerText = calculate();
+                    const empty = output.innerText;
+                    if (empty === '' ) {
+                        return;
+                    }
+                    let right = operation.slice(operatorIndex +1)
+                    operand2 = parseFloat(right.join(''));
+                    result = output.innerText = calculate();
+                    operation.push(result);
                 } else {
-                    output.innerText += value;
+                    result = output.innerText += value;
+                    operation.push(result);
                 }
-        
             }
         );
     }
 );
 
 const calculate = () => {
-    
     if (operator === '+') {
         return operand1 + operand2;
     } else if (operator === '-') {
