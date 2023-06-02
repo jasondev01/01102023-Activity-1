5c2d99996feafc0440f948b18d38fb8bcd592aaa
Jason Ruben <jsnrbn01@gmail.com>
2023-06-02 20:07:18 +0800

    updated the elements and js algorithms

--- calculator.js
+++ calculator.js
@@ -1,71 +1,119 @@
+// leftOperand, operator, rightOperand
+//      1           +           2
+
 const output = document.querySelector('.output-value');
-const buttons = document.querySelectorAll('button');
+const equals = document.querySelector('.equals');
+const decimal = document.querySelector('.decimal');
+const clear = document.querySelector('.clear');
+const del = document.querySelector('.del');
 
-let operation = [];
-const operatorArray = ['+', '-', '*', '/'];
+const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operators');
 
-buttons.forEach(
-    (button) => {
-        button.addEventListener( 
-            'click',
+const operation = [];
+const operatorsArr = [ '+', '-', 'x', '÷' ];
+
+
+addToDisplay = (e) => {
+    operation.push(e.target.value)
+    output.innerText += e.target.value
+}
+
+decimal.addEventListener( 'click', addToDisplay );
+numbers.forEach( number => { number.addEventListener( 'click', addToDisplay )} )
+
+operators.forEach( 
+    operator => {
+        operator.addEventListener( 
+            'click', 
             (e) => {
-                let value = e.target.value;
-                let result;
                 let operatorIndex;
-                if (value === 'clear') {
-                    output.innerText = '';
-                    operand1 = '';
-                    operand2 = '';
-                    operator = undefined;
-                } else if (value == 'delete') {
-                    let string = output.innerText.toString();
-                    output.innerText = string.slice(0, -1);
-                } else if (value === '+' || value === '-' || value === 'x' || value === '/') {
-                    operator = value;
-                    for ( let value of operatorArray) {
-                        const index = operation.indexOf(value)
-                        if ( index == value) {
-                            operatorIndex = index;
-                            break;
-                        }
+                for( let operator of operatorsArr ) {
+                    const index = operation.indexOf(operator); // returns -1 if false
+                    if ( index > -1 ) {
+                        operatorIndex = index;
+                        break;
                     }
-                    let left = operation.slice(0, operatorIndex)
-                    operand1 = parseFloat(left.join(''));
-                    result = output.innerText += value;
-                    operation.push(result);
-                } else if (value === '.' && output.innerText.includes('.')) {
+                }
+                if (operation.includes(operation[operatorIndex])) {
                     return;
-                } else if (value === '=') {
-                    const empty = output.innerText;
-                    if (empty === '' ) {
-                        return;
-                    }
-                    let right = operation.slice(operatorIndex +1)
-                    operand2 = parseFloat(right.join(''));
-                    result = output.innerText = calculate();
-                    operation.push(result);
                 } else {
-                    result = output.innerText += value;
-                    operation.push(result);
+                    operation.push(e.target.value)
+                    output.innerText += e.target.value
                 }
             }
-        );
-    }
-);
+        )
+    } 
+)
 
-const calculate = () => {
-    if (operator === '+') {
-        return operand1 + operand2;
-    } else if (operator === '-') {
-        return operand1 - operand2;
-    } else if (operator === 'x') {
-        return operand1 * operand2;
-    } else if (operator === '/') {
-        if (operand2 === 0) {
-            return "Cannot divide by zero";
+clear.addEventListener( 
+    'click', 
+    () => {
+        output.innerText = '';
+        operation.length = 0;
+    }
+)
+
+del.addEventListener(
+    'click',
+    () => {
+        let number = output.innerText; 
+        if ( !number ) {
+            return;
         } else {
-            return operand1 / operand2;
+            let newNumber = parseFloat(number.slice(0, -1));
+            if ( isNaN(newNumber) ) {
+                newNumber = '';
+            }
+            output.innerText = newNumber; 
+            operation.length = 0;
+            operation.push(newNumber);
         }
     }
-}
-calculate();
+)
+
+equals.addEventListener(
+    'click',
+    () => {
+        let operatorIndex;
+        let result;
+
+        for( let operator of operatorsArr ) {
+            const index = operation.indexOf(operator); // returns -1 if false
+            if ( index > -1 ) {
+                operatorIndex = index;
+                break;
+            }
+        }
+        // [ 4, 5, 1, +, 9, 9, 9 ] = operatorIndex = 3
+        // leftNumber = 451, rightNumber = 999
+        const left = operation.slice(0, operatorIndex); // from index[0] to index[operatorIndex] = [ 4,5,1 ]
+        const right = operation.slice(operatorIndex + 1); // from operatorIndex (3+1=4 so index[4]) = [ 9,9,9 ]
+        const leftNumber = parseFloat( left.join('') ); // left.join returns string hence the parseFloat
+        const rightNumber = parseFloat( right.join('') ); // ('') separator is important if not it will have comma
+        const operator = operation[operatorIndex]; // get the operator = (-)
+
+        if ( right.length === 0 || !operator) {
+            return;
+        } else {
+            switch( operator ) {
+                case '+': 
+                    result = leftNumber + rightNumber
+                    break;
+                case '-':
+                    result = leftNumber - rightNumber
+                    break;
+                case 'x':
+                    result = leftNumber * rightNumber
+                    break;
+                case '÷':
+                    result = leftNumber / rightNumber
+                    break;
+            }
+            output.innerText = result;
+            operation.length = 0;
+            operation.push(result);
+        } 
+        
+    }
+)
